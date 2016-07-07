632b24b1219060b5fb2ec756643152c8c6caff5f
Kathy H <kathykehuang@gmail.com>
2016-07-07 15:17:28 -0400

    Add menu_order attributes

--- WordPress/Query posts order by custom fields/index.php
+++ WordPress/Query posts order by custom fields/index.php
@@ -1,19 +1,35 @@
 <?php 
 
 // get posts - orderby start_date
-$posts = get_posts(array(
+$args = array(
 	'post_type'			=> 'event',
 	'posts_per_page'	=> -1,
 	'meta_key'			=> 'start_date',
 	'orderby'			=> 'meta_value_num',
 	'order'				=> 'DESC'
-));
+);
+
+$posts = get_posts( $args );
 
 // WP Query - orderby featured
-$the_query = new WP_Query(array(
+$args = array(
 	'post_type'			=> 'event',
 	'posts_per_page'	=> -1,
 	'meta_key'			=> 'featured',
 	'orderby'			=> 'meta_value_num',
 	'order'				=> 'DESC'
-));
+);
+
+$the_query = new WP_Query( $args );
+
+//Orderby WordPress default page attribute 'order'
+//Display posts sorted by 'menu_order' with a fallback to post 'title', in a descending order:
+
+$args = array(
+	'orderby' => 'menu_order title',
+	'order'   => 'DESC',
+);
+
+$query = new WP_Query( $args );
+
+//Reference link: https://codex.wordpress.org/Class_Reference/WP_Query
